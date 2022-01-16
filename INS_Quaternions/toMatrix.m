d079e969f0afc16b2becf659c8434b029e12c389
Alex <Alex@CanekComp>
2022-01-16 14:43:51 +0300

    INS models

---
+++ INS_Quaternions/toMatrix.m
@@ -0,0 +1,10 @@
+function M = toMatrix( q )
+
+    M = [
+        q(1) -q(2) -q(3) -q(4);
+        q(2)  q(1) -q(4)  q(3);
+        q(3)  q(4)  q(1) -q(2);
+        q(4) -q(3)  q(2)  q(1)
+        ];
+end
+
